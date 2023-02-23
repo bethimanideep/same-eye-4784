bf6bf039edc37085eae7a4b794e03dbd458517f0
bethimanideep <bethimanideep1999@gmail.com>
2023-02-23 23:58:29 +0530

    day-2

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const express = require("express")
 const path = require("path")
 const passport = require("passport")
+const bcrypt=require("bcrypt")
 const session = require("express-session")
 const app = express()
 const cors=require("cors")
+const { connection, temp, model } = require("./configs/db")
+const { validatoor } = require("./middleware/validator")
 app.use(cors())
 require("./auth")
 app.use(express.json())
@@ -46,14 +49,86 @@ app.get('/auth/google/callback',
 app.get("/auth/google/failure", (req, res) => {
     res.send("went wrong")
 })
-app.get("/auth/protected", isLoggedIn, (req, res) => {
+app.get("/auth/protected", isLoggedIn, async(req, res) => {
     let name=req.user
-    res.send(`Hello ${JSON.stringify(name)}`)
+    let data={
+        username:req.user.displayName,
+        email:req.user.email,
+        image:req.user.picture,
+        type:"google"
+    }
+    if(name){
+        let user=await model.findOne({email:data.email})
+        if(user==null){
+            let dbuser=new model(data)
+            await dbuser.save()
+            let tempuser=new temp(data)
+            await temp.deleteMany()
+            await tempuser.save()
+            res.redirect("http://127.0.0.1:5501/frontend/temp.html")
+        }else{
+            await temp.deleteMany()
+            let tempuser=new temp(data)
+            await tempuser.save()
+            res.redirect("http://127.0.0.1:5501/frontend/temp.html")
+        }
+    }
 })
 app.get("/auth/logout", isLoggedIn, (req, res) => {
     req.session.destroy()
     res.send("bye")
 })
-app.listen(5000, () => {
+app.get("/tempuser",async(req,res)=>{
+    let user=await temp.find()
+    if(user.length===0){
+        res.json("false")
+    }else{
+        res.json(user[0])
+    }
+})
+app.post("/adduser",async(req,res)=>{
+    let email=req.body.email
+    let password=req.body.password
+    let user=await model.findOne({email})
+    if(user){
+        res.json("exists")
+    }else{
+        let hash=bcrypt.hashSync(password,5)
+        req.body.password=hash
+        let user=new model(req.body)
+        await user.save()
+        res.json("success");
+    }
+    
+})
+app.post("/loginuser",async(req,res)=>{
+    let email=req.body.email
+    let user=await model.findOne({email})
+    if(user){
+        await temp.deleteMany()
+        res.json(user)
+    }else{
+        res.json("notexists")
+    }
+})
+app.post("/verify",async(req,res)=>{
+    let email=req.body.email
+    let user=await model.findOne({email})
+    if(user){
+        res.json("matched")
+    }else{
+        res.json("notmatched")
+    }
+})
+app.patch("/resetpassword",validatoor, async (req, res) => {
+    res.json("success")
+})
+app.listen(5000, async() => {
+    try {
+        await connection
+        console.log("connected to db");
+    } catch (error) {
+        console.log(error);
+    }
     console.log("running at 5000");
 })
